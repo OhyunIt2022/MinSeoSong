1d98ecf06dd5ed0f953ed3570dc601f59c8f3b2b
minseo707 <ministiler0515@gmail.com>
2022-07-25 17:47:27 +0900

    파이썬 자료 추가

---
+++ kakaoSolution.py
@@ -0,0 +1,26 @@
+def solution(id_list, report, k):
+    id_report = dict(); reports = []; banList = []
+    attackId = [0 for x in range(len(id_list))]
+    id_reporter = dict()
+    for x in id_list:
+        id_report[x] = set()
+        id_reporter[x] = set()
+        
+    for x in report:
+        dx = x.split()
+        id_report[dx[1]].add(dx[0])
+        id_reporter[dx[0]].add(dx[1])
+        
+    for key, value in id_report.items():
+        reports.append(len(value))
+    
+    for x in range(len(reports)):
+        if reports[x] >= k:
+            banList.append(list(id_report.keys())[x])
+            
+    for i in banList:
+        for x in range(len(id_list)):
+            if i in list(id_reporter.values())[x]:
+                attackId[x] += 1
+        
+    return attackId
